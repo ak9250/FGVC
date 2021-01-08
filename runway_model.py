48ca476d33965e86f0648041160810b6243fae84
ak9250 <ahsen.khaliq@gmail.com>
2021-01-08 15:01:40 -0500

    setup runway

---
+++ runway_model.py
@@ -0,0 +1,54 @@
+import runway
+import numpy as np
+import shutil
+import os
+import os.path as osp
+import platform
+import argparse
+import time
+import sys
+from iPERCore.services.run_imitator import run_imitator
+import subprocess
+from subprocess import call
+from PIL import Image
+import shutil
+
+def run_cmd(command):
+    try:
+        print(command)
+        call(command, shell=True)
+    except KeyboardInterrupt:
+        print("Process interrupted")
+        sys.exit(1)
+    
+
+@runway.command('imitate', inputs={'source': runway.image, 'target': runway.image}, outputs={'image': runway.image})
+def imitate(models, inputs):
+  os.makedirs('images', exist_ok=True)
+  inputs['source'].save('images/temp1.jpg')
+  inputs['target'].save('images/temp2.jpg')
+
+  paths1 = os.path.join('images','temp1.jpg')
+  paths2 = os.path.join('images','temp2.jpg')
+
+  src_path = "./images/temp1.jpg"
+
+  ref_path = "./images/temp2.jpg"
+
+  counter = 0
+  trump = 0
+  akun = 0
+  stage_1_command = ("python video_completion.py"
+            + " --mode object_removal"
+            + " --path src_path"
+            + " --path_mask ref_path"
+            + " --outroot ../result/temp_removal"
+            + " --seamless"
+  )      
+  run_cmd(stage_1_command)
+  path = "../result/temp_removal/"
+  img = Image.open(open(path, 'rb'))
+  return img
+
+if __name__ == '__main__':
+    runway.run()
