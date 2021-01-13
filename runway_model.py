ba3a7c4aecc2fd004168f79f1c7a09ed7cf59aba
Ahsen <ahsen.khaliq@gmail.com>
2021-01-12 19:15:02 -0500

    object removal only

--- runway_model.py
+++ runway_model.py
@@ -46,23 +46,23 @@ def removal(models, inputs):
   img = Image.open(open(path, 'rb'))
   return img
 
-@runway.command('extrapolation', inputs={'source': runway.image,}, outputs={'image': runway.image})
-def extrapolation(models, inputs):
-  os.makedirs('../images', exist_ok=True)
+# @runway.command('extrapolation', inputs={'source': runway.image,}, outputs={'image': runway.image})
+# def extrapolation(models, inputs):
+#   os.makedirs('../images', exist_ok=True)
 
-  inputs['source'].save('../images/00000.png')
+#   inputs['source'].save('../images/00000.png')
 
-  stage_1_command = ("python video_completion.py"
-            + " --mode video_extrapolation"
-            + " --path ../images"
-            + " --outroot ../result/temp_extrapolation"
-            + " --H_scale 2"
-            + " --W_scale 2"
-  )          
-  run_cmd(stage_1_command)
-  path = "../result/temp_extrapolation/frame_comp_final/00000.png"
-  img = Image.open(open(path, 'rb'))
-  return img
+#   stage_1_command = ("python video_completion.py"
+#             + " --mode video_extrapolation"
+#             + " --path ../images"
+#             + " --outroot ../result/temp_extrapolation"
+#             + " --H_scale 2"
+#             + " --W_scale 2"
+#   )          
+#   run_cmd(stage_1_command)
+#   path = "../result/temp_extrapolation/frame_comp_final/00000.png"
+#   img = Image.open(open(path, 'rb'))
+#   return img
 
 
 if __name__ == '__main__':
